ccf4acb7d6045bb4f432f342b2d6ee145f7962dc
fishseabowl <xuefeih@jfrog.com>
2022-06-09 13:42:45 -0700

    Async Analysis version 1

--- async_analy/src/main.rs
+++ async_analy/src/main.rs
@@ -1,41 +1,30 @@
 use futures::future::join_all;
-use log::info;
-use pretty_env_logger;
-use std::collections::HashMap;
-use std::sync::{Arc, Mutex, RwLock};
-use std::thread::{self, sleep};
-use std::time::Duration;
-use tokio::task;
+use log::{error, info};
+use pretty_env_logger::env_logger;
+use std::thread;
+use tokio::*;
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
 
-fn slowwly(delay_sec: u64) -> reqwest::Url {
-    let url = "http://www.google.com";
-    sleep(Duration::new(delay_sec, 0));
+fn slowwly(delay_ms: u32) -> reqwest::Url {
+    let url = "https://jfrog.com";
+    thread::sleep(time::Duration::from_millis(delay_ms as u64));
     reqwest::Url::parse(&url).unwrap()
 }
 
 // Now we want to both fetch some data and do some CPU intensive analysis on it
-async fn get_and_analyze(n: u8, data: Arc<RwLock<HashMap<u8, u8>>>) -> Result<(u64, u64)> {
-    let response: reqwest::Response = reqwest::get(slowwly(1)).await?;
+async fn get_and_analyze(n: usize) -> Result<(u64, u64)> {
+    let response: reqwest::Response = reqwest::get(slowwly(1000)).await?;
+
     info!("Dataset {}", n);
 
     // we get the body of the request
     let txt = response.text().await?;
 
-    let mut map = data.write().expect("RwLock poisoned");
-
-    // We use HashMap::entry to handle the case where another thread
-    // inserted the same key while where were unlocked.
-
-    map.entry(n).or_insert_with(|| n * 2 + 1);
-    // Let the loop start us over to try again
-
     // We send our analysis work to a thread where there is no runtime running
     // so we don't block the runtime by analyzing the data
     let res = task::spawn_blocking(move || analyze(&txt)).await?;
     info!("Processed {}", n);
-
     Ok(res)
 }
 
@@ -52,27 +41,12 @@ fn analyze(txt: &str) -> (u64, u64) {
     (ones, zeros)
 }
 
-#[tokio::main]
-async fn main() -> Result<()> {
-    pretty_env_logger::init();
+async fn app() -> Result<()> {
     // This is new. We can collect futures in a collection. Nice to know!
     let mut futures = vec![];
 
-    let mut map = Arc::new(RwLock::new(HashMap::new()));
-
-    let mut mymap = map.write().expect("RwLock poisoned");
-    mymap.insert(1, 15);
-    drop(mymap);
-    info!("Start process!!");
     for i in 1..=10 {
-        let fut = task::spawn(move || {
-            async {
-                let data = Arc::clone(&map);
-
-                get_and_analyze(i, data)
-            }
-            .await;
-        });
+        let fut = task::spawn(get_and_analyze(i));
         futures.push(fut);
     }
 
@@ -97,4 +71,14 @@ async fn main() -> Result<()> {
         total_ones as f64 / total_zeros as f64
     );
     Ok(())
+}
+
+fn main() {
+    env_logger::init();
+    let mut rt = tokio::runtime::Runtime::new().unwrap();
+
+    match rt.block_on(app()) {
+        Ok(_) => info!("Done"),
+        Err(e) => error!("An error ocurred: {}", e),
+    };
 }
