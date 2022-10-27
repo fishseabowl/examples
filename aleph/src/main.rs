4f59da04a1425e159651c69b13f82f93e576cbb9
fishseabowl <xuefeih@jfrog.com>
2022-10-27 13:05:52 -0700

    Add clap parser

--- aleph/src/main.rs
+++ aleph/src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+use aleph_bft::{run_session, NodeIndex, Terminator};
+use clap::Parser;
+use tokio;
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// Index of the node
+    #[clap(long, value_parser)]
+    id: usize,
+
+    /// Ports
+    #[clap(long, value_parser, value_delimiter = ',')]
+    ports: Vec<usize>,
+
+    /// Number of items to be ordered
+    #[clap(long, value_parser)]
+    n_data: u32,
+
+    /// Number of the first created item
+    #[clap(default_value = "0", long, value_parser)]
+    n_starting: u32,
+
+    /// Indices of nodes having stalling DataProviders
+    #[clap(default_value = "", long, value_parser, value_delimiter = ',')]
+    stalled: Vec<usize>,
+
+    /// Should the node crash after finalizing its items
+    #[clap(long, value_parser)]
+    crash: bool,
+}
+
+#[tokio::main]
+async fn main() {
+    pretty_env_logger::init_timed();
 }
